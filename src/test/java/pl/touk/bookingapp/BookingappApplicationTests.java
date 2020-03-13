0ec755b4575f004651ec62425d7bdb5570cba831
k-wasilewski <k.k.wasilewski@gmail.com>
2020-03-13 08:37:38 +0100

    initial commit

---
+++ src/test/java/pl/touk/bookingapp/BookingappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.touk.bookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
