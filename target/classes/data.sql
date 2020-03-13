36d22dc9840827a62eb02593b49f9fa93a702cf3
k-wasilewski <k.k.wasilewski@gmail.com>
2020-03-13 12:02:54 +0100

    removed target

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO movies(name, date, time) VALUES('Pan samochodzik', '2019-06-15', '12:00:00');
-INSERT INTO movies(name, date, time) VALUES('Imperium kontratakuje', '2019-12-15', '22:00:00');
-INSERT INTO movies(name, date, time) VALUES('Ogniem i mieczem', '2019-12-15', '08:00:00');
-INSERT INTO movies(name, date, time) VALUES('Najlepszy', '2020-03-13', '12:00:00');
